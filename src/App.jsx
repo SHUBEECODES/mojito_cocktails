0e66df2cd18a95df12fef4d86382649e63951e69
shubhangi <shubhangigarg71@gmail.com>
2025-06-24 16:56:24 +0530

    implemented the first part of the hero section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { ScrollTrigger,SplitText }from 'gsap/all'
 import gsap from 'gsap'
 import Navbar from './Component/Navbar'
+import Hero from './Component/Hero'
 
 
 
@@ -12,6 +13,8 @@ const App = () => {
   return (
   <main>
  <Navbar/>
+ <Hero/>
+ <div className='h-dvh bg-black'></div>
   </main>
   )
 }
